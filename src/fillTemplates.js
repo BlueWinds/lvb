eca87c3d9f08981d6896e1804f8a76221195ee52
BlueWinds <admin@winds.blue>
2019-06-20 10:04:11 -0700

    Standalone mode, rework everything

--- src/fillTemplates.js
+++ src/fillTemplates.js
@@ -1,17 +1,22 @@
 const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
-const mustache = require('mustache')
+const Handlebars = require('handlebars')
 const mkdirp = require('mkdirp')
 const rimraf = require('rimraf')
 
-const substitutions = {}
-const partials = {}
+const defines = require('./defines')
+
+const nicePrint = (n) => n.toFixed(3).replace(/0+$/, '')
+
+Handlebars.registerPartial('test_partial', "partial - {{parameter}}")
+Handlebars.registerHelper('getWeight', (mult) => nicePrint(1 + parseFloat(mult, 10)))
+Handlebars.registerHelper('multiply', (base, mult) => nicePrint(parseFloat(base, 10) * parseFloat(mult, 10)))
 
 // Read partial templates
 for (const file of glob.sync('**/*.txt', {cwd: path.resolve(__dirname, 'partials')})) {
   const text = fs.readFileSync(path.resolve('src/partials', file)).toString()
-  partials[file.split('.')[0]] = text
+  Handlebars.registerPartial(file.split('.')[0], text)
 }
 
 rimraf.sync(path.resolve(__dirname, '../mod/!!!LV Blue Edits/*'))
@@ -21,7 +26,7 @@ for (const file of glob.sync('**/*.{txt,yml,gfx,gui}', {cwd: path.resolve(__dirn
   const text = fs.readFileSync(path.resolve('src/templates', file)).toString()
   const outPath = path.resolve(__dirname, '../mod/!!!LV Blue Edits', file)
   mkdirp.sync(path.dirname(outPath))
-  fs.writeFileSync(outPath, mustache.render(text, substitutions, partials))
+  fs.writeFileSync(outPath, Handlebars.compile(text)(defines))
 }
 
 // Copy over dds files
